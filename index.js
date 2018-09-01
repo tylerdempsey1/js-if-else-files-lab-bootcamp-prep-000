b5a649456dc2d4e9fdf2cd5e6b6bc670c9f31f07
Tyler Dempsey <tylerdempsey1@hotmail.com>
2018-09-01 19:24:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ var height = "74"
 
 if (name === "Susan") {
   console.log("the name is correct")
-else {
+} else {
   console.log("Expected the name to be Susan")
 }
-}
-
 // Don't worry about this
 module.exports = { name, height
-}
